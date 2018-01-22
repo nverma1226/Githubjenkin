a6369b7f31eebce0dc16d3c38a6bfd180712a0e6
Neerajverma <neeraj.verma@intsof.com>
2018-01-22 14:46:44 +0530

    test

---
+++ NewJavaProject/src/Package1/Class_one.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class Class_one {
+
+	@Test
+	public void method1()
+	{
+		
+		System.out.println("This is class_one");
+	}
+}
---
+++ NewJavaProject/src/Package1/Class_two.java
@@ -0,0 +1,13 @@
+package Package1;
+
+import org.testng.annotations.Test;
+
+public class Class_two {
+
+	@Test
+	public void method1()
+	{
+		
+		System.out.println("This is class_two");
+	}
+}
